3a9f0f56e41e61131ed3f94acd24ad25577ac2a1
devesh lamichhane <deveshl@protonmail.com>
2021-12-29 18:47:32 +0545

    updated Slider

--- slider/src/App.tsx
+++ slider/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from "./data";
 import Slider from "./Slider";
 
@@ -11,20 +11,45 @@ export interface IData {
 }
 const dataLength = data.length;
 
+// JS gives out negetive module for some reason
+// this function will correct that
+const moduloDL = (x: number): number =>
+  ((x % dataLength) + dataLength) % dataLength;
+
 function App() {
   const [num, setNum] = useState<number>(0);
-  const goRight = () =>
-    setNum(num === dataLength - 1 ? num + 1 - dataLength : num + 1);
-  const goLeft = () => setNum(num === 0 ? num - 1 + dataLength : num - 1);
+  const [counting, setCounting] = useState<boolean>(true);
+
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setNum((prevNum) => moduloDL(prevNum + 1));
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [num]);
+
   return (
     <div>
       <header>
         <h1>/ Reviews</h1>
       </header>
 
-      <button onClick={goRight}>&lt;</button>
+      <button
+        onClick={() => {
+          setNum((prevNum) => moduloDL(prevNum + 1));
+          setCounting(false);
+        }}
+      >
+        &lt;
+      </button>
+      <button
+        onClick={() => {
+          setNum((prevNum) => moduloDL(prevNum - 1));
+          setCounting(false);
+        }}
+      >
+        &gt;
+      </button>
       <Slider person={data[num]} />
-      <button onClick={goLeft}>&gt;</button>
     </div>
   );
 }
--- slider/src/Slider.tsx
+++ slider/src/Slider.tsx
@@ -1,14 +1,17 @@
 import React, { ReactElement } from "react";
 import { IData } from "./App";
 
+const picStyle = {
+  height: "150px",
+  width: "150px",
+  border: "6px blue solid",
+  borderRadius: "150px",
+};
+
 const Slider = ({ person }: { person: IData }): ReactElement => {
   return (
     <div>
-      <img
-        src={person.image}
-        alt={person.name}
-        style={{ height: "150px", width: "150px" }}
-      />
+      <img src={person.image} alt={person.name} style={picStyle} />
       <h4>{person.name}</h4>
       <p>{person.title}</p>
       <p>{person.quote}</p>
